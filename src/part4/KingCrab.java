3aeeb4130940fed5e526ea3e02b198a326f94243
esimm98 <esimm98@gmail.com>
2016-03-01 09:47:33 -0700

    KingCrab complete

--- src/part4/KingCrab.java
+++ src/part4/KingCrab.java
@@ -8,8 +8,16 @@ import java.util.ArrayList;
 public class KingCrab extends CrabCritter {
 	
 	public void processActors(ArrayList<Actor> actors) {
+		Grid gr = getGrid();
 		for (Actor a : actors) {
-			
+			Location loc = a.getLocation();
+			int dir = getLocation().getDirectionToward(loc);
+			Location nextLoc = loc.getAdjacentLocation(dir);
+			if (gr.isValid(nextLoc) && gr.get(nextLoc) == null) {
+				a.moveTo(nextLoc);
+			} else {
+				a.removeSelfFromGrid();
+			}
 		}
 	}
 	
